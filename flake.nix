c29e35cf81ae92cc7a66444beae250f88313641f
Oscar Molnar <oscar@tymscar.com>
2024-04-05 04:28:29 +0100

    Add flake for nix development

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Tymscar Blog";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in { default = pkgs.mkShell { buildInputs = [ pkgs.hugo ]; }; });
+    };
+}
